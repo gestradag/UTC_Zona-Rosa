df6f5c88455ec6aaac9f68934628b456768479d0
gestradag <52520459+gestradag@users.noreply.github.com>
2021-03-08 19:44:16 -0600

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+numero1 = float( input("Introduce el 1er. número: ") )
+numero2 = float( input("Introduce el 2do. número: ") )
+
+print("Los números son iguales? ", numero1 == numero2)
+print("Los números son diferentes?", numero1 != numero2)
+print("El 1ro. es mayor que el 2do.?", numero1 > numero2)
+print("El 2do. es mayor o igual que el 1ro.?", numero1 <= numero2)
